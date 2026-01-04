76283558798a0c970f741139b635b29713d00f9d
aubin-pierre7 <aubinp58@gmail.com>
2026-01-04 23:08:46 +0100

    Modifications du h1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App(props) {
 
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>Hello, world! Ici c'est Sawa</h1>
       <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
     </div>
   );
